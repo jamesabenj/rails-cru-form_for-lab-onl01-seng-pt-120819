e827f97040c3cf1cad575b300950d4416a882981
James Benjamin <james.abenj@gmail.com>
2020-05-21 16:37:22 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,30 @@
+class SongsController < ApplicationController
+  def new
+  end
+
+  def create
+    @song = Song.new(song_params(:name))
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:name))
+    redirect_to artist_path(@song)
+  end
+
+  private
+
+    def song_params(*args)
+      params.require(:song).permit(*args)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  resources :artists, only: [:new, :edit]
+  get '/artists/:id', to: 'artists#show', as: 'artist'
+  post '/artists/new', to: 'artists#create'
+  patch '/artists/:id', to: 'artists#update'
+
+  resources :songs, only: [:new, :create, :edit]
+  get '/songs/:id', to: 'songs#show', as: 'song'
+  patch '/songs/:id', to: 'songs#update'
+
+  resources :genres, only: [:new, :edit]
+  get '/genres/:id', to: 'genres#show', as: 'genre'
+  post '/genres/new', to: 'genres#create'
+  patch '/genres/:id', to: 'genres#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
