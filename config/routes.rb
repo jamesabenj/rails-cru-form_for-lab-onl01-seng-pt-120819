201a67956a27f7e1f0babfa168c9fa033d7009f0
James Benjamin <james.abenj@gmail.com>
2020-05-21 17:24:12 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  resources :artists, only: [:new, :edit]
+  get '/artists/:id', to: 'artists#show', as: 'artist'
+  post '/artists/new', to: 'artists#create'
+  patch '/artists/:id', to: 'artists#update'
+
+  resources :songs, only: [:index, :new, :edit]
+  get '/songs/:id', to: 'songs#show', as: 'song'
+  post '/songs/new', to: 'songs#create'
+  patch '/songs/:id', to: 'songs#update'
+
+  resources :genres, only: [:new, :edit]
+  get '/genres/:id', to: 'genres#show', as: 'genre'
+  post '/genres/new', to: 'genres#create'
+  patch '/genres/:id', to: 'genres#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
